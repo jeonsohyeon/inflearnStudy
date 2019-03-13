b6a869151d16e2b324f91cd2effc138feff192ba
JeonSoHyeon <sohyeon@dfy.co.kr>
2019-03-13 11:52:38 +0900

    에러 수정 및 코드 개선

--- 190310/app_file.js
+++ 190310/app_file.js
@@ -4,45 +4,47 @@ const fs = require('fs');
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({extended:false}));
-
+app.locals.pretty = true;
 app.set('views', './views_file');
 app.set('view engine', 'pug');
-app.locals.pretty = true;
+app.get('/topic/new', function(req, res){
+	res.render('new');
+})
 
+app.post('/topic', function(req, res){
+	let title = req.body.title;
+	let description = req.body.description;
+	fs.writeFile('data/'+title, description, function(err){
+		if(err){
+			res.status(500).send('internal server Error')
+		}
+		res.redirect('/topic/'+title)
+	})
+})
 app.listen(3000, function(){
-    console.log('http://localhost:3000')
+	console.log('http://localhost:3000')
 })
 
-app.get('/', function(req, res){
-    res.render('index');
+app.get(['/topic', '/topic/:id'], function(req, res){ //라우터 링크도 배열로 받아올 수 있음
+	let id = req.params.id;
+	fs.readdir('data', function(err, files){
+		if(err){
+			res.status(500).send('internal server Error')
+		}
+		if(id){
+			fs.readFile('data/'+id, 'utf8', function(err, data){
+				res.render('view', {
+					fileName: files,
+					title : id,
+					description : data
+				})
+			})
+		}else{
+			res.render('view', {
+				fileName : files,
+				title:'null',
+				description:'null'
+			})
+		}
+	})
 })
-app.post('/', function(req, res){
-    let tit = req.body.title;
-    let desc = req.body.description;
-    fs.writeFile(`data/${tit}`, desc, function(err){
-        if(err) res.status(500).send('internal server Error')
-        //req.send('success!')
-    })
-})
-app.get('/views', function(req, res){
-    fs.readdir('data', function(err, files){
-        if(err) alert('err')
-        res.render('view', {
-            fileName : files
-        })
-    })
-})
-
-app.get('/views/:title', function(req, res){
-    let id = req.params.id;
-    fs.readdir('data', function(err, files){
-        if(err) alert('err')
-        fs.readFile('data/'+id, 'utf8', function(err, data){
-            res.render('view', {
-                fileName: files,
-                title : id,
-                description : data
-            })
-        })
-    })
-});
