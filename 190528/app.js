a77eaa509e740a2d24876b4d73fa3e302e7b33c2
JeonSoHyeon <sohyeon@dfy.co.kr>
2019-05-30 14:52:00 +0900

    mysql 연동

--- 190528/app.js
+++ 190528/app.js
@@ -1,6 +1,16 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var mysql = require('mysql');
 var app = express();
+
+var connection = mysql.createConnection({
+	host : '127.0.0.1',
+	port : 3000,
+	user : 'ROOT',
+	password : '87852560',
+	database : 'js'
+})
+connection.connect();
 
 app.use(express.static('public'));
 app.use(bodyParser.json())
@@ -19,12 +29,29 @@ app.get('/form', function(req, res){
 	res.sendFile(__dirname+"/public/form.html");
 });
 
-app.post('/email_post', function(req, res){
+app.post('/email_post', function(req, res){ 
 	res.render('email.ejs', {'email': req.body.email})
 })
 
 app.post('/ajax_send_email', function(req, res){
-	//input value validation. => db 조회 기능 추가
-	var responseData = { 'result' : 'ok', 'email' : req.body.email};
-	res.json(responseData)
+	//데이터베이스가 없을 경우 데이터를 이렇게 보내지만
+	// var responseData = { 'result' : 'ok', 'email' : req.body.email};
+	// res.json(responseData)
+	//mysql 연동 한 경우 쿼리 날리기.
+	var email = req.body.email;
+	var responseData = {};
+	var query = connection.query(`select name from user where email='${email}'`, function(err, rows){
+		connection.release();
+		if(err) throw err;
+		if(rows[0]){
+			console.log('있음');
+			responseData.result = "ok";
+			responseData.name = rows[0].name;
+		}else{
+			console.log('없음');
+			responseData.result = "none";
+			responseData.name = '';
+		}
+		res.json(responseData);
+	})
 })
